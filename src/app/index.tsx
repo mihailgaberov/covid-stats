ea79cf1318e40c7867422abe2ae450042a3ab84a
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-10-08 18:20:35 +0300

    :sparkles: Added close state icon for the menu button.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { faCog, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 import { Dashboard } from './components/popup';
 import { Settings } from './components/popup';
 import { Container, MenuButton, DashboardHead, SectionTitle } from 'styles';
 
 const App: FC = () => {
-    const [ settingsOpened, setSettingsOpened ] = useState(false);
+    const [settingsOpened, setSettingsOpened] = useState(false);
 
     return (
         <Container className="ashiishme_covid_container">
@@ -16,7 +16,10 @@ const App: FC = () => {
                     <SectionTitle>COVID-19 STATS</SectionTitle>
                 </div>
                 <MenuButton onClick={() => settingsOpened ? setSettingsOpened(false) : setSettingsOpened(true)}>
-                    <FontAwesomeIcon icon={faCog} />
+                    {settingsOpened ?
+                        <FontAwesomeIcon icon={faTimesCircle} /> :
+                        <FontAwesomeIcon icon={faCog} />
+                    }
                 </MenuButton>
             </DashboardHead>
             {settingsOpened ? <Settings /> : <Dashboard />}
